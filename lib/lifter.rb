eef81674b2df7eeed5aadb394e882ca7a03a6ade
taniabelalov <belalov.tania16@gmail.com>
2019-09-02 19:33:05 -0400

    implamention

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,39 @@
 class Lifter
+
   attr_reader :name, :lift_total
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.lifter == self}
+  end 
+
+  def gyms
+    memberships.map {|membership| membership.lifter}
+  end
+
+  def self.average_lift
+    total_lift_amount = self.all.inject(0) {|sum, lifter| sum + lifter.lift_total}
+    total_lifters = self.all.count
+    percent = (total_lift_amount / total_lifters)*100
+  end
+
+  def total_cost
+    memberships.inject(0) {|sum, membership| sum + membership.cost}
+  end 
+
+  def sign_in(gym, cost)
+    Membership.new(cost, self, gym)
+  end 
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,23 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+l1 = Lifter.new("Ruby", 34)
+l2 = Lifter.new("Arnold", 154)
+l3 = Lifter.new("Rocki", 122)
+l4 = Lifter.new("Ben", 32)
+
+gym1 = Gym.new("Yes")
+gym2 = Gym.new("You can do it!")
+gym3 = Gym.new("Lose weight")
+gym4 = Gym.new("just LIFT")
+
+m1 = Membership.new(1040, l1, gym1)
+m2 = Membership.new(1007, l2, gym2)
+m3 = Membership.new(106, l3, gym3)
+m4 = Membership.new(1043, l4, gym4)
+m5 = Membership.new(1080, l1, gym3)
+m6 = Membership.new(1090, l2, gym4)
+
 
 binding.pry
 
