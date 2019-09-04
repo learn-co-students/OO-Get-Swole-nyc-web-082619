195aaecabf35d016335837a7a09faf69ef1e29df
github username <github email address>
2019-09-04 09:38:33 -0400

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,35 @@
 class Lifter
   attr_reader :name, :lift_total
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership|membership.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map{|membership|membership.gym}.uniq
+  end
+
+  def self.lift_totals
+    self.all.map{|lifter|lifter.lift_total}
+  end
+
+  def self.average_lift_total
+    self.lift_totals.reduce(:+)/self.all.length.to_f
+  end
+
+  def sign_up_to_new_gym(gym, cost)
+    Membership.new(cost, gym, self)
   end
 end
